8c8580dc6417c6bf2e23842e95a5ab0383172174
thryec <thryec@gmail.com>
2022-05-22 14:11:33 +0800

    complete counter contract and compiled to teal

--- scripts/2-first-transaction.js
+++ scripts/2-first-transaction.js
@@ -1,26 +1,28 @@
-import dotenv from "dotenv";
-import algosdk from "algosdk";
-dotenv.config();
+import dotenv from 'dotenv'
+import algosdk from 'algosdk'
+dotenv.config()
 
-let myAccount = algosdk.mnemonicToSecretKey(process.env.ACCOUNT_MNEMONIC);
+let myAccount = algosdk.mnemonicToSecretKey(process.env.ACCOUNT_MNEMONIC)
 
-const baseServer = "https://testnet-algorand.api.purestake.io/ps2";
-const port = "";
+const baseServer = 'https://testnet-algorand.api.purestake.io/ps2'
+const port = ''
 const token = {
-  "X-API-Key": process.env.API_KEY,
-};
-const algodClient = new algosdk.Algodv2(token, baseServer, port);
+  'X-API-Key': process.env.API_KEY,
+}
 
-(async () => {
+// This variable is our client. It is the link between our code and the blockchain
+const algodClient = new algosdk.Algodv2(token, baseServer, port)
+
+;(async () => {
   try {
-    let params = await algodClient.getTransactionParams().do();
-    console.log("params: ", params);
+    let params = await algodClient.getTransactionParams().do()
+    console.log('params: ', params)
     const receiver =
-      "HZ57J3K46JIJXILONBBZOHX6BKPXEM2VVXNRFSUED6DKFD5ZD24PMJ3MVA";
-    const enc = new TextEncoder();
-    const note = enc.encode("My first transaction on Algo!");
-    let amount = 100000; // equals .1 ALGO
-    let sender = myAccount.addr;
+      'HZ57J3K46JIJXILONBBZOHX6BKPXEM2VVXNRFSUED6DKFD5ZD24PMJ3MVA'
+    const enc = new TextEncoder()
+    const note = enc.encode('My first transaction on Algo!')
+    let amount = 100000 // equals .1 ALGO
+    let sender = myAccount.addr
 
     let txn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
       from: sender,
@@ -28,28 +30,27 @@ const algodClient = new algosdk.Algodv2(token, baseServer, port);
       amount: amount,
       node: note,
       suggestedParams: params,
-    });
+    })
 
-    console.log("HERE3");
-
-    let accountInfo = await algodClient.accountInformation(myAccount.addr).do();
-    console.log("Account balance: %d microAlgos", accountInfo.amount);
+    let accountInfo = await algodClient.accountInformation(myAccount.addr).do()
+    console.log('Account balance: %d microAlgos', accountInfo.amount)
 
     // sign transaction
-    let signedTxn = txn.signTxn(myAccount.sk);
-    let txId = txn.txID().toString();
-    console.log("Signed transaction with txID: %s", txId);
+    let signedTxn = txn.signTxn(myAccount.sk)
+    console.log('signed transaction:', signedTxn)
+    let txId = txn.txID().toString()
+    console.log('Signed transaction with txID: %s', txId)
 
-    await algodClient.sendRawTransaction(signedTxn).do();
+    await algodClient.sendRawTransaction(signedTxn).do()
 
     // Wait for confirmation
-    let confirmedTxn = await algosdk.waitForConfirmation(algodClient, txId, 4);
-    accountInfo = await algodClient.accountInformation(myAccount.addr).do();
-    console.log("Transaction Amount: %d microAlgos", confirmedTxn.txn.txn.amt);
-    console.log("Transaction Fee: %d microAlgos", confirmedTxn.txn.txn.fee);
-    console.log("Account balance: %d microAlgos", accountInfo.amount);
+    let confirmedTxn = await algosdk.waitForConfirmation(algodClient, txId, 4)
+    accountInfo = await algodClient.accountInformation(myAccount.addr).do()
+    console.log('Transaction Amount: %d microAlgos', confirmedTxn.txn.txn.amt)
+    console.log('Transaction Fee: %d microAlgos', confirmedTxn.txn.txn.fee)
+    console.log('Account balance: %d microAlgos', accountInfo.amount)
   } catch (err) {
-    console.error("Failed to get apps from the sdk", err);
-    process.exit(1);
+    console.error('Failed to get apps from the sdk', err)
+    process.exit(1)
   }
-})();
+})()
